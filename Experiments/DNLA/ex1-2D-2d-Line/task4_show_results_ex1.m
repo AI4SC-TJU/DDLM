08e826a374641e27d8f918f432955a2e875d0e05
Haotian Yi <93070782+ccdzz@users.noreply.github.com>
2023-11-23 15:48:49 +0800

    Update task4_show_results_ex1.m

--- Experiments/DNLA/ex1-2D-2d-Line/task4_show_results_ex1.m
+++ Experiments/DNLA/ex1-2D-2d-Line/task4_show_results_ex1.m
@@ -1,4 +1,13 @@
 % plot figures one by one
+% Figure 5 (Fig5) - DN-PINNs Result
+% To obtain the data represented in Figure 5, execute the script `DN-PINNs-2prob-2D.py`.
+% Figure 6 (Fig6) - DNLA (PINNs) Results
+% For the results displayed in Figure 6, execute the script `DNLM-2prob-2D-DNLM(PINN).py` to generate the corresponding data.
+% Figure 7 (Fig7) - DNLA (Ritz) Results
+% To generate the results shown in Figure 7, run the script `DNLM-2prob-2D-DNLM(Ritz).py`.
+% Figure Generation:
+% Utilize MATLAB and execute the script `plot_Solutions_1by1.m` to create the graphical representation associated with the data obtained from the previous steps.
+% Please ensure that all necessary dependencies and prerequisites for the Python scripts and MATLAB files are correctly installed and configured before executing the scripts.
 
 format short e
 close all
